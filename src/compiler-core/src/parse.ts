abcee18af87a3a7a3c871da9b1ab3089d8e3c051
黄立根 <476421978@qq.com>
2023-06-10 10:11:17 +0800

    46-实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -8,32 +8,47 @@ const enum TagType {
 export function baseParse(content) {
   const context = createParseContext(content)
 
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any = []
 
-  // {{}}
-  let node
-  const s = context.source
-  if (s.startsWith("{{")) {
-    node = parseInterpolation(context)
-  } else if (s[0] === "<") {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context)
+  while (!isEnd(context, ancestors)) {
+    let node
+    const s = context.source
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context)
+    } else if (s[0] === "<") {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
     }
-  } else {
-    node = parseText(context)
+    if (!node) {
+      node = parseText(context)
+    }
+    nodes.push(node)
   }
-
-  nodes.push(node)
-
   return nodes
 }
 
 function parseText(context) {
-  const content = parseTextData(context, context.source.length)
+  const endTokens = ["<", "{{"]
+  let endIndex = context.source.length
+
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i])
+    // endIndex > index 是需要要 endIndex 尽可能的小
+    // 比如说：
+    // hi, {{123}} <div></div>
+    // 那么这里就应该停到 {{ 这里，而不是停到 <div 这里
+    if (index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+  }
+
+  const content = parseTextData(context, endIndex)
+
   return {
     type: NodeType.TEXT,
     content,
@@ -47,7 +62,9 @@ function parseTextData(context: any, length) {
 }
 
 function parseInterpolation(context) {
-  // {{ message }}
+  // 1. 先获取到结束的index
+  // 2. 通过 closeIndex - startIndex 获取到内容的长度 contextLength
+  // 3. 通过 slice 截取内容
 
   const openDelimiter = "{{"
   const closeDelimiter = "}}"
@@ -58,16 +75,17 @@ function parseInterpolation(context) {
     closeDelimiter.length
   )
   // 去掉{{
-  context.source = context.source.slice(openDelimiter.length) // message}}
+  advanceBy(context, 2)
 
   // 获取 中间内容长度
   const rawContentLength = closeIndex - openDelimiter.length // message的长度
+  const rawContent = context.source.slice(0, rawContentLength)
 
-  const rawContent = parseTextData(context, rawContentLength) // 获取message内容
-  const content = rawContent.trim()
+  const preTrimContent = parseTextData(context, rawContent.length) // 获取message内容
+  const content = preTrimContent.trim()
 
-  // 移动下标到最右边表示结束
-  context.source = advanceBy(context, rawContentLength + closeDelimiter.length)
+  // 最后在让代码前进2个长度，可以把 }} 干掉
+  advanceBy(context, closeDelimiter.length)
 
   return {
     type: NodeType.INTERPOLATION,
@@ -94,28 +112,77 @@ function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length)
 }
 
-function parseElement(context: any) {
+function parseElement(context: any, ancestors) {
+  // 应该如何解析 tag 呢
+  // <div></div>
+  // 先解析开始 tag
   // 1.解析tag
-  const element = parseTag(context, TagType.START) // 开始标签
-  parseTag(context, TagType.END) // 结束标签
-  // context.source 为空表示处理完成
-  console.log("——————", context.source)
+  const element: any = parseTag(context, TagType.START) // 开始标签
 
+  ancestors.push(element)
+
+  const children = parseChildren(context, ancestors)
+
+  ancestors.pop()
+
+  // 解析 end tag 是为了检测语法是不是正确的
+  // 检测是不是和 start tag 一致
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.END) // 结束标签
+  } else {
+    throw new Error(`缺失结束标签:${element.tag}`)
+  }
+
+  // 正常结束标签才添加children
+  element.children = children
+
+  // context.source 为空表示处理完成
   return element
 }
 
 function parseTag(context: any, type: TagType) {
   const match: any = /^<\/?([a-z]*)/i.exec(context.source)
-  //  [ '<div', 'div', index: 0, input: '<div></div>', groups: undefined ]
   const tag = match[1]
-  // 2.删除处理完的代码
-  advanceBy(context, match[0].length) // ></div>
-  advanceBy(context, 1) // </div>
+  // 移动光标
+  // <div
+  advanceBy(context, match[0].length)
+  // 暂时不处理 selfClose 标签的情况 ，所以可以直接 advanceBy 1个坐标 <  的下一个就是 >
+  advanceBy(context, 1)
 
   if (type === TagType.END) return
 
   return {
     type: NodeType.ELEMENT,
-    tag: tag,
+    tag,
   }
+}
+
+function isEnd(context: any, ancestors) {
+  // 2、遇到结束标签
+  const s = context.source
+  // </div>
+  if (startsWith(s, `</`)) {
+    // 从后面往前面查
+    // 因为便签如果存在的话 应该是 ancestors 最后一个元素
+    for (let i = ancestors.length - 1; i >= 0; --i) {
+      if (startsWithEndTagOpen(s, ancestors[i].tag)) {
+        return true
+      }
+    }
+  }
+
+  return !s
+}
+
+function startsWithEndTagOpen(source: string, tag: string) {
+  // 1. 头部 是不是以  </ 开头的
+  // 2. 看看是不是和 tag 一样
+  return (
+    startsWith(source, "</") &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+  )
+}
+
+function startsWith(source: string, searchString: string): boolean {
+  return source.startsWith(searchString)
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -21,6 +21,7 @@ describe("Parse", () => {
       expect(ast.children[0]).toStrictEqual({
         type: NodeType.ELEMENT,
         tag: "div",
+        children: [],
       })
     })
   })
@@ -28,11 +29,64 @@ describe("Parse", () => {
   describe("text", () => {
     it("simple text", () => {
       const ast: any = baseParse("some text")
-
       expect(ast.children[0]).toStrictEqual({
         type: NodeType.TEXT,
         content: "some text",
       })
+    })
+    // 综合
+    test("hello world", () => {
+      const ast: any = baseParse("<div>hi,{{message}}</div>")
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeType.ELEMENT,
+        tag: "div",
+        children: [
+          {
+            type: NodeType.TEXT,
+            content: "hi,",
+          },
+          {
+            type: NodeType.INTERPOLATION,
+            content: {
+              type: NodeType.SIMPLE_INTERPOLATION,
+              content: "message",
+            },
+          },
+        ],
+      })
+    })
+
+    test("Nest element", () => {
+      const ast: any = baseParse("<div><p>hi</p>{{message}}</div>")
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeType.ELEMENT,
+        tag: "div",
+        children: [
+          {
+            type: NodeType.ELEMENT,
+            tag: "p",
+            children: [
+              {
+                type: NodeType.TEXT,
+                content: "hi",
+              },
+            ],
+          },
+          {
+            type: NodeType.INTERPOLATION,
+            content: {
+              type: NodeType.SIMPLE_INTERPOLATION,
+              content: "message",
+            },
+          },
+        ],
+      })
+    })
+
+    test("should throw error when lack end tag  ", () => {
+      expect(() => {
+        baseParse("<div><span></div>")
+      }).toThrow("缺失结束标签:span")
     })
   })
 })
