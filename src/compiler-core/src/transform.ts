6f814b379573258fdb4603e86550fa62d5517d11
黄立根 <476421978@qq.com>
2023-06-12 20:26:56 +0800

    48-实现transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,37 @@
+export function transform(root, options) {
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
+}
+
+function traverseNode(node: any, context) {
+  // if (node.type === NodeType.TEXT) {
+  //   node.content = node.content + " mini-vue"
+  // }
+  // 优化变成外部扩展
+  const nodeTransforms = context.nodeTransforms
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  traverseChildren(node, context)
+}
+
+// 深度优先搜索 递归
+function traverseChildren(node: any, context: any) {
+  const children = node.children
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  }
+  return context
+}
